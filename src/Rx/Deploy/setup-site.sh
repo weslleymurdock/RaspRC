68a00dddaeeeab918b7c038afbbe2934b7a986ed
Weslley Luiz <weslleyluiz@outlook.com.br>
2024-09-05 07:28:23 -0300

    change creation of virtualhost file

--- src/Rx/Deploy/setup-site.sh
+++ src/Rx/Deploy/setup-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo touch /etc/apache2/sites-available/rasp.local.conf
-sudo echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/rasp.local.conf
+sudo echo "<VirtualHost *:80>" > /etc/apache2/sites-available/rasp.local.conf
 sudo echo "    ProxyPreserveHost On" >> /etc/apache2/sites-available/rasp.local.conf
 sudo echo "    ProxyPass / http://127.0.0.1:5217/" >> /etc/apache2/sites-available/rasp.local.conf
 sudo echo "    ProxyPassReverse / http://127.0.0.1:5217/" >> /etc/apache2/sites-available/rasp.local.conf
