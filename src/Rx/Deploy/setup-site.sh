0c852abfa854647f3c3df5cbfa7f0358f92f72ff
Weslley Luiz <weslleyluiz@outlook.com.br>
2024-09-05 05:44:31 -0300

    create site through script echos

--- src/Rx/Deploy/setup-site.sh
+++ src/Rx/Deploy/setup-site.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-sudo mv -v ~/rasp.local.conf /etc/apache2/sites-available/
+sudo touch /etc/apache2/sites-available/rasp.local.conf
+sudo echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/rasp.local.conf
+sudo echo "    ProxyPreserveHost On" >> /etc/apache2/sites-available/rasp.local.conf
+sudo echo "    ProxyPass / http://127.0.0.1:5217/" >> /etc/apache2/sites-available/rasp.local.conf
+sudo echo "    ProxyPassReverse / http://127.0.0.1:5217/" >> /etc/apache2/sites-available/rasp.local.conf
+sudo echo "    ErrorLog ${APACHE_LOG_DIR}/rx-error.log" >> /etc/apache2/sites-available/rasp.local.conf
+sudo echo "    CustomLog ${APACHE_LOG_DIR}/rx-access.log common" >> /etc/apache2/sites-available/rasp.local.conf
+sudo echo "</VirtualHost>" >> /etc/apache2/sites-available/rasp.local.conf
+
+
