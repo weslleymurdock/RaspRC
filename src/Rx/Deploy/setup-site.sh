9c4df4b055d640d2313962256c9bbeaaa702b0b2
Weslley Luiz <weslleyluiz@outlook.com.br>
2024-09-05 06:27:52 -0300

    fix permissions on folder

--- src/Rx/Deploy/setup-site.sh
+++ src/Rx/Deploy/setup-site.sh
@@ -8,5 +8,6 @@ sudo echo "    ProxyPassReverse / http://127.0.0.1:5217/" >> /etc/apache2/sites-
 sudo echo "    ErrorLog ${APACHE_LOG_DIR}/rx-error.log" >> /etc/apache2/sites-available/rasp.local.conf
 sudo echo "    CustomLog ${APACHE_LOG_DIR}/rx-access.log common" >> /etc/apache2/sites-available/rasp.local.conf
 sudo echo "</VirtualHost>" >> /etc/apache2/sites-available/rasp.local.conf
-
+sudo usermod -a -G www-data rx
+sudo chown -R -f www-data:www-data /var/www/rasprc-rx
 mv -v $1/publish/* /var/www/rasprc-rx
